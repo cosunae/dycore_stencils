ad2d3d561203c95f21cc560a71fb7fd6728b9c18
Mauro Bianco <mbianco@cscs.ch>
2016-07-05 15:31:51 +0200

    anable caches for vertical

--- vertical_advection/functions.hpp
+++ vertical_advection/functions.hpp
@@ -20,14 +20,14 @@ GT_FUNCTION void backward_sweep(const unsigned int i,
     if (is_in_domain< 0, 0, 0, 0 >(iblock_pos, jblock_pos, block_size_i, block_size_j)) {
 
         datacol[index(i, j, k, strides)] = dcol[index(i, j, k, strides)];
-        ccol[index(i, j, k, strides)] = datacol[index(i, j, k, strides)];
+        //ccol[index(i, j, k, strides)] = datacol[index(i, j, k, strides)];
         utens_stage_ref[index(i, j, k, strides)] =
             DTR_STAGE * (datacol[index(i, j, k, strides)] - u_pos[index(i, j, k, strides)]);
         // kbody
         for (k = domain.m_k - 2; k >= 0; --k) {
             datacol[index(i, j, k, strides)] =
                 dcol[index(i, j, k, strides)] - (ccol[index(i, j, k, strides)] * datacol[index(i, j, k + 1, strides)]);
-            ccol[index(i, j, k, strides)] = datacol[index(i, j, k, strides)];
+            //ccol[index(i, j, k, strides)] = datacol[index(i, j, k, strides)];
             utens_stage_ref[index(i, j, k, strides)] =
                 DTR_STAGE * (datacol[index(i, j, k, strides)] - u_pos[index(i, j, k, strides)]);
         }
@@ -55,23 +55,44 @@ GT_FUNCTION void forward_sweep(const unsigned int i,
     // k minimum
     int k = 0;
 
+#ifdef USE_CACHE_
+    Real ccol_cache[2];
+    Real dcol_cache[2];
+#endif
+
     if (is_in_domain< 0, 0, 0, 0 >(iblock_pos, jblock_pos, block_size_i, block_size_j)) {
 
         Real gcv =
             (Real)0.25 * (wcon[index(i + ishift, j + jshift, k + 1, strides)] + wcon[index(i, j, k + 1, strides)]);
         Real cs = gcv * BET_M;
 
+#ifdef USE_CACHE_
+        ccol_cache[0] = gcv * BET_P;
+        Real bcol = DTR_STAGE - ccol_cache[0];
+#else
         ccol[index(i, j, k, strides)] = gcv * BET_P;
         Real bcol = DTR_STAGE - ccol[index(i, j, k, strides)];
-
+#endif
         // update the d column
         Real correctionTerm = -cs * (u_stage[index(i, j, k + 1, strides)] - u_stage[index(i, j, k, strides)]);
-        dcol[index(i, j, k, strides)] = DTR_STAGE * u_pos[index(i, j, k, strides)] + utens[index(i, j, k, strides)] +
+#ifdef USE_CACHE_
+        dcol_cache[0]
+#else
+        dcol[index(i, j, k, strides)]
+#endif
+            = DTR_STAGE * u_pos[index(i, j, k, strides)] + utens[index(i, j, k, strides)] +
                                         utens_stage_ref[index(i, j, k, strides)] + correctionTerm;
 
         Real divided = (Real)1.0 / bcol;
+#ifdef USE_CACHE_
+        ccol_cache[0] = ccol_cache[0] * divided;
+        ccol[index(i, j, k + 1, strides)] = ccol_cache[0];
+        dcol_cache[0] *= divided;
+        dcol[index(i, j, k + 1, strides)] = dcol_cache[0];
+#else
         ccol[index(i, j, k, strides)] = ccol[index(i, j, k, strides)] * divided;
         dcol[index(i, j, k, strides)] = dcol[index(i, j, k, strides)] * divided;
+#endif
     }
 
     // kbody
@@ -86,19 +107,42 @@ GT_FUNCTION void forward_sweep(const unsigned int i,
             Real cs = gcv * BET_M;
 
             Real acol = gav * BET_P;
+#ifdef USE_CACHE_
+            ccol_cache[1] = gcv * BET_P;
+            Real bcol = DTR_STAGE - acol - ccol_cache[1];
+#else
             ccol[index(i, j, k, strides)] = gcv * BET_P;
             Real bcol = DTR_STAGE - acol - ccol[index(i, j, k, strides)];
+#endif
 
             Real correctionTerm = -as * (u_stage[index(i, j, k - 1, strides)] - u_stage[index(i, j, k, strides)]) -
                                   cs * (u_stage[index(i, j, k + 1, strides)] - u_stage[index(i, j, k, strides)]);
-            dcol[index(i, j, k, strides)] = DTR_STAGE * u_pos[index(i, j, k, strides)] +
-                                            utens[index(i, j, k, strides)] + utens_stage_ref[index(i, j, k, strides)] +
-                                            correctionTerm;
+#ifdef USE_CACHE_
+            dcol_cache[1]
+#else
+            dcol[index(i, j, k, strides)]
+#endif
+                = DTR_STAGE * u_pos[index(i, j, k, strides)] +
+                utens[index(i, j, k, strides)] + utens_stage_ref[index(i, j, k, strides)] +
+                correctionTerm;
 
+#ifdef USE_CACHE_
+            Real divided = (Real)1.0 / (bcol - (ccol_cache[0] * acol));
+            ccol_cache[1] = ccol_cache[1] * divided;
+
+            ccol_cache[1] = ccol_cache[1] * divided;
+            ccol[index(i, j, k + 1, strides)] = ccol_cache[0];
+            ccol_cache[0]=ccol_cache[1];
+
+            dcol_cache[1] = (dcol_cache[1] - (dcol_cache[0] * acol)) * divided;
+            dcol[index(i, j, k + 1, strides)] = dcol_cache[0];
+            dcol_cache[0]=dcol_cache[1];
+#else
             Real divided = (Real)1.0 / (bcol - (ccol[index(i, j, k - 1, strides)] * acol));
             ccol[index(i, j, k, strides)] = ccol[index(i, j, k, strides)] * divided;
             dcol[index(i, j, k, strides)] =
                 (dcol[index(i, j, k, strides)] - (dcol[index(i, j, k - 1, strides)] * acol)) * divided;
+#endif
         }
     }
 
@@ -117,7 +161,11 @@ GT_FUNCTION void forward_sweep(const unsigned int i,
         dcol[index(i, j, k, strides)] = DTR_STAGE * u_pos[index(i, j, k, strides)] + utens[index(i, j, k, strides)] +
                                         utens_stage_ref[index(i, j, k, strides)] + correctionTerm;
 
+#ifdef USE_CACHE_
+        Real divided = (Real)1.0 / (bcol - (ccol_cache[0] * acol));
+#else
         Real divided = (Real)1.0 / (bcol - (ccol[index(i, j, k - 1, strides)] * acol));
+#endif
         dcol[index(i, j, k, strides)] =
             (dcol[index(i, j, k, strides)] - (dcol[index(i, j, k - 1, strides)] * acol)) * divided;
     }
